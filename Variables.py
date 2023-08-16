0c2b4cf506b0f8cf4173e869aa6ea06c4c9b14ce
AlinaTr <atrandafir78@gmail.com>
2023-08-16 12:31:05 +0300

    created file variables

---
+++ Variables.py
@@ -0,0 +1,13 @@
+# Initialize a variable
+
+car_brand = 'Volvo'
+car_model = 'XC 60'
+
+favorite_fruit = 'apple'
+
+prop1 = 'value'
+prop_2 = 'prop2'
+
+# example of case-sensitive variable
+my_var = 5
+my_Var = 10
